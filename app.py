596e92e710789f8a5422b7ef94898b0194dfc438
Maxime <maxime.olivier@sekoia.fr>
2018-02-08 09:30:05 +0100

    Flask API initialization

---
+++ app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# System imports
+import json
+import sys
+import os.path
+
+# API imports
+from flask import Flask
+
+# Business imports
+from lib.core.methods import *
+from lib.core.search import Search
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, welcome to the vFeed API"
+
+@app.route('/vfeed/api/cve/<cve_id>', methods=['GET'])
+def get_cve_detail(cve_id):
+    return CveInfo(cve).get_cve()
+
+@app.route('vfeed/api/cpe/<cpe_id>', methods=['GET'])
+def get_cpe(cpe):
+    return Search(cpe).cpe()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
